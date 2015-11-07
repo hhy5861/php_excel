c1c6cd7bdb6aab6b8e40963d16cf4c89cdae8a1e
Mike <hhy5861@163.com>
2015-11-07 21:35:33 +0800

    kris

--- index_browser.php
+++ index_browser.php
@@ -4,19 +4,30 @@ require_once "vendor/autoload.php";
 use lib\ExportDataExcel;
 
 $excel = new ExportDataExcel('browser','/Users/Mike/Downloads/');
-$excel->filename = "test.xls";
-
-$data = array(
-    array(1,2,3),
-    array("asdf","jkl","semi"),
-    array("1273623874628374634876","=asdf","10-10"),
-    array("2010-01-02 10:00AM","1/1/11","10-10"),
-    array("1234","12.34","-123."),
-    array("-12345678901234567890","0.0000000000123456789","-"),
-);
+$excel->filename = rand(1,100) . ".xls";
 
 $excel->initialize();
-foreach($data as $row) {
+
+for($i = 1; $i<50000; $i++)
+{
+    $row = array($i, genRandomString(), genRandomString());
     $excel->addRow($row);
 }
+
+/*foreach($data as $row)
+{
+    $excel->addRow($row);
+}*/
 $excel->finalize();
+
+
+function genRandomString($length = 100)
+{
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyz _";
+    $string = "";
+    for ($p = 0; $p < $length; $p++)
+    {
+        $string .= $characters[mt_rand(0, strlen($characters)-1)];
+    }
+    return $string;
+}
